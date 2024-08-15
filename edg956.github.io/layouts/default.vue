59aea3e3e0760d671843531592d135961efa8868
Eugenio Doñaque <eugenio.donaque@ontruck.com>
2024-08-16 00:44:41 +0200

    Fixed responsive header

--- edg956.github.io/layouts/default.vue
+++ edg956.github.io/layouts/default.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <b-navbar type="is-dark" class="pl-6 pr-6">
+    <b-navbar fixed-top="true" type="is-dark" class="pl-6-tablet pr-6-tablet">
       <template #start>
-        <b-navbar-item tag="router-link" :to="{path: '/'}">
+        <b-navbar-item class="is-hidden-mobile" tag="router-link" :to="{path: '/'}">
       	  <b-image src="https://github.com/edg956.png" alt="EU" rounded></b-image>
-	</b-navbar-item>
+        </b-navbar-item>
       </template>
       <template #end>
-	<b-navbar-item tag="router-link" :to="{path: '/'}">Home</b-navbar-item>
-	<b-navbar-item tag="router-link" :to="{path: '/curriculum'}">Experience</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{path: '/'}">Home</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{path: '/curriculum'}">Experience</b-navbar-item>
       </template>
     </b-navbar>
     <Nuxt />
     <footer class="footer">
       <div class="content has-text-centered">
       	<a href="https://www.flaticon.com/free-icons/letter-d" title="letter d icons">Letter d icons created by Kian Bonanno - Flaticon</a>
-	<p>Site created with <a href="https://buefy.org/">Buefy</a> and <a href="https://nuxtjs.org/">Nuxt.js</a></p>
+	      <p>Site created with <a href="https://buefy.org/">Buefy</a> and <a href="https://nuxtjs.org/">Nuxt.js</a></p>
       </div>
     </footer>
   </div>
